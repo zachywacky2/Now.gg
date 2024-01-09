d215e1f9f1e057f9e0778700698b43e804ad9336
astralevolution <admin@astralevolution.pro>
2024-01-09 18:05:18 -0500

    fix deployments

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/astralevolution/',
+    bare: 'https://tomp.http.astralevolution.pro', 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
