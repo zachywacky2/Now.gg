f56f7b30943d8020b0fa795b0f0b495a263ac64f
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2024-05-27 18:23:47 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/astralevolution/',
-    bare: 'https://tomp.http.astralevolution.pro', 
+    bare: 'https://juniorhighmaths.tafca.co.uk/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
